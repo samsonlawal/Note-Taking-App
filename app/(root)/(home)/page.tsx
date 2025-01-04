f2f0e43d31eee44899e830c591ecaf245da7791c
deji <lawal4211@gmail.com>
2025-01-04 20:02:52 +0100

    next top loader

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   // );
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between w-full">
+    <main className="flex max-h-screen flex-col items-center justify-between w-full h-fit">
       <HomePage />
     </main>
   );
