dddaafb340d215531ed906ffe8554b5e313546b9
deji <lawal4211@gmail.com>
2025-01-26 03:02:27 +0100

    image sections

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,13 +6,15 @@ import { useEffect } from "react";
 import router from "next/router";
 import HomePage from "@/components/ui/HomePage";
 import Navbar from "@/components/ui/Navbar";
+import Peek from "@/components/ui/HomePage/peek";
 
 export default function Home() {
   const { accessToken } = useAuth();
 
   return (
-    <main className="flex max-h-screen flex-col items-center justify-between w-full magicpattern dark:bg-gray-800">
+    <main className="flex max-h-screen h-fit flex-col items-center justify-between w-full magicpattern dark:bg-gray-800">
       <HomePage />
+      <Peek />
     </main>
   );
 }
