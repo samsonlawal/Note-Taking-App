be5bf7bd56f765690c7d6a64c741560222ea713e
deji <lawal4211@gmail.com>
2025-02-24 22:08:20 +0100

    auth mobile

--- app/(root)/auth/login/page.tsx
+++ app/(root)/auth/login/page.tsx
@@ -2,7 +2,7 @@ import BasicForm from "@/components/auth/pages/login";
 
 function LoginForm() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between px-24 py-[50px]">
+    <div className="flex min-h-screen flex-col items-center justify-between px-4 md:px-24 py-[50px]">
       <BasicForm />
     </div>
   );
--- app/(root)/auth/sign-up/page.tsx
+++ app/(root)/auth/sign-up/page.tsx
@@ -1,7 +1,7 @@
 import BasicForm from "@/components/auth/pages/sign-up";
 function SignUpForm() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between px-24 py-[50px]">
+    <div className="flex min-h-screen flex-col items-center justify-between px-4 md:px-24 py-[50px]">
       <BasicForm />
     </div>
   );
